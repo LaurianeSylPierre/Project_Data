fc56d8c0a5a19af1dc40d0347b0d471b20e16263
Farrah Ryan <ryanfarrah25410@gmail.com>
2017-03-01 10:26:18 +0100

    Add index.php redirect to view/index.php

---
+++ view/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require "model/pdo.php";
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+  <form action="" method="post">
+
+    <label for="name">Name or pseudo</label>
+    <input type="text" name="name">
+
+  </form>
+
+</body>
+
+</html>
