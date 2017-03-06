9f65d1ff32c60f88eb7f8613c299dbe1093e2c04
Unknown <laurianepierre@outlook.fr>
2017-03-06 14:54:40 +0100

    insert of quel_mort

--- view/chose.php
+++ view/chose.php
@@ -5,6 +5,6 @@
     </div>
     <div class="row">
         <a href="index.php?yd" class="col offset-s1 s5 valign-wrapper brown lighten-5 to_know"><p class="valign center-align">To know how you will die</p></a>
-        <a href="index.php?x.php" class="col s5 valign-wrapper orange lighten-5 to_chose"><p class="valign center-align">To chose how you will die</p></a>
+        <a href="index.php?wdyp" class="col s5 valign-wrapper orange lighten-5 to_chose"><p class="valign center-align">To chose how you will die</p></a>
     </div>
 </article>
