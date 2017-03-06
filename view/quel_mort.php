f077aa9caf495ae36141d7822e5c8646c9bbd95b
Unknown <sonya.grosjean0195@gmail.com>
2017-03-06 14:54:15 +0100

    Changes quel_mort

--- view/quel_mort.php
+++ view/quel_mort.php
@@ -13,12 +13,11 @@
 <body>
 	<article class="container">
     <div class="row center">
-        <h1>Chose your destiny</h1>
-        <p>Do you prefere...</p>
+        <h2>Do you prefere...</h2>
     </div>
     <div class="row">
-        <a href="index.php?yd" class="col offset-s1 s5 valign-wrapper brown lighten-5 to_know"><p class="valign center-align">To know how you will die</p></a>
-        <a href="index.php?x.php" class="col s5 valign-wrapper orange lighten-5 to_chose"><p class="valign center-align">To chose how you will die</p></a>
+        <a href="index.php?yd" class="col offset-s1 s5 valign-wrapper brown lighten-5 to_know"><p class="valign center-align">blopblopblop</p></a>
+        <a href="index.php?x.php" class="col s5 valign-wrapper orange lighten-5 to_chose"><p class="valign center-align">blopblopblop</p></a>
     </div>
 </article>
 </body>
