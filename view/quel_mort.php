f639ddbc47512a7dac83fef12b766b5653c6e72c
Unknown <sonya.grosjean0195@gmail.com>
2017-03-06 14:31:40 +0100

    Will be changed in two minutes

--- view/quel_mort.php
+++ view/quel_mort.php
@@ -5,10 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="view/materialize/css/materialize.min.css"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="view/style.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="style1.css"  media="screen,projection"/>
     <title>Document</title>
 </head>
 
 <body>
+	<header>
+        <nav>
+            <div class="nav-wrapper brown lighten-3">
+                <a href="#" class="brand-logo center">Tu préfères...</a>
+            </div>
+        </nav>
+    </header>
 	
+	<div class="container">
+		<div class="row">
+			<div class="valign-wrapper color">
+				<button type="submit" class="col s5 waves-effect waves-light btn blue-grey lighten-1">blablalbalblal</button>
+				<div class="col s2 center">
+					<p>Ou</p>
+				</div>
+				<button type="submit" class="col s5 waves-effect waves-light btn blue-grey lighten-1">blablalbalba</button>
+			</div>
+		</div>
+	</div>
+</body>
