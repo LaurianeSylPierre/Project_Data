1cc9a5dee43ae142fbb4afc797a58873e3ecbb8a
Unknown <sonya.grosjean0195@gmail.com>
2017-03-06 10:46:15 +0100

    File Creation

---
+++ view/quel_mort.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="view/materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="view/style.css"  media="screen,projection"/>
+    <title>Document</title>
+</head>
+
+<body>
+	
