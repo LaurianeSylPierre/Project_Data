b999c50d39c7a8f02bf47efa4319d337bc7f667c
Unknown <laurianepierre@outlook.fr>
2017-03-07 13:46:40 +0100

    couleurs de css

--- view/chose.php
+++ view/chose.php
@@ -4,7 +4,7 @@
         <p>Do you prefere...</p>
     </div>
     <div class="row">
-        <a href="index.php?yd" class="col offset-s1 s5 valign-wrapper brown lighten-5 to_know"><p class="valign center-align">To know how you will die</p></a>
+        <a href="index.php?yd" class="col offset-s1 s5 valign-wrapper brown lighten-4 to_know"><p class="valign center-align">To know how you will die</p></a>
         <a href="index.php?wdyp" class="col s5 valign-wrapper orange lighten-5 to_chose"><p class="valign center-align">To chose how you will die</p></a>
     </div>
 </article>
