46d0dda2f983aad86f427f1f5567256cf959b9d1
Unknown <quangnhanbui@gmail.com>
2017-03-08 11:50:15 +0100

    better disease game

---
+++ controller/ajax.js
@@ -0,0 +1,30 @@
+
+        function get_poll(){
+            $("ul").html("");
+            $.ajax({
+                type: "POST",
+                url: "ajax.php",
+                data: "act=getq",
+                dataType: "json",
+                success: function(response){
+                    var ans1, ans = "";
+                    if($(".poll")).html().length)
+                    $(".poll").css("display, none");
+                    if(response.id){
+                        ans+='<li class="list-group-item"><div class="radio"><label><input class="rad" type="radio" name="poll_options" id="'+i+'" value="'+i+'"><label for="'+i+'">'+val+'</label></label></div></li>';
+
+                    });
+                    $('.poll').html('');
+                    $(".question").html(response.question);
+                    $("ul").append(ans);
+
+                }else{
+                    $(".next").remove();
+                    $(".question").html(response.question);
+                    $(".button").remove();
+                    $(".poll").fadeIn("slow").html("<span class="err">plus de questions :(</scan>)")
+                }
+
+                }
+            });
+        }
